960e5b5cc724eefc4de13c14031da8f09405bb8e
Joey Cozza <joeycozza@gmail.com>
2019-02-14 15:01:41 -0700

    tweaking an import rule

--- recommended.js
+++ recommended.js
@@ -3,7 +3,6 @@ module.exports = {
   extends: ['frontier/es6', 'frontier/react', 'plugin:jest/recommended'],
   rules: {
     "import/no-unresolved": 2,
-    "import/extensions": [2, "always"],
     "import/named": 2,
     "import/default": 2,
     "import/no-absolute-path": 2,
