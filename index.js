20cab12d17bac5439431df4cf22abf4703d43f3c
Joey Cozza <joeycozza@gmail.com>
2019-06-14 13:44:01 -0600

    moving jsdoc rules and usage of the plugin to a separate file

--- index.js
+++ index.js
@@ -109,18 +109,5 @@ module.exports = {
     'no-undefined': 'off',
     'no-unused-vars': 'warn',
     'no-use-before-define': ['error', {functions: false}],
-
-    //jsdoc Section
-    'jsdoc/check-param-names': 2,
-    'jsdoc/check-tag-names': 2,
-    'jsdoc/check-types': 2,
-    'jsdoc/newline-after-description': 0,
-    'jsdoc/require-description-complete-sentence': 2,
-    'jsdoc/require-hyphen-before-param-description': 2,
-    'jsdoc/require-param': 2,
-    'jsdoc/require-param-description': 2,
-    'jsdoc/require-param-type': 2,
-    'jsdoc/require-returns-description': 2,
-    'jsdoc/require-returns-type': 2
   }
 };
